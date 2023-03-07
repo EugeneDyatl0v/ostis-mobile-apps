2e6c7cb213d4e9ec9020bf6f8dcc992a40eeae02
Eugene Dyatlov <91074872+EugeneDyatl0v@users.noreply.github.com>
2023-03-07 14:27:23 +0300

    Add files via upload

---
+++ ostis-example-app/build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/eugene/Загрузки/ostis-example-app
+# Build directory: /home/eugene/Загрузки/ostis-example-app/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("ostis-web-platform/sc-machine/thirdparty")
+subdirs("ostis-web-platform/sc-machine/sc-memory")
+subdirs("ostis-web-platform/sc-machine/sc-kpm")
+subdirs("ostis-web-platform/sc-machine/sc-tools")
+subdirs("problem-solver")
